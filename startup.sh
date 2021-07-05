2566ade0f39f9801c99020b55945177b8e7a9071
Mark Turner <mark.turner-7@postgrad.manchester.ac.uk>
2021-07-05 11:22:32 +0100

    Fixed IndexError

--- startup.sh
+++ startup.sh
@@ -2,4 +2,4 @@ mkdir /etc/cron.hourly
 echo "#!/bin/sh" > /etc/cron.hourly/update.sh
 echo "python3 -u /ceah-backend/update.py $DB_USERNAME $DB_PASSWORD" >> /etc/cron.hourly/update.sh
 chmod +x /etc/cron.hourly/update.sh
-python3 -u main.py
+python3 -u main.py $DB_USERNAME $DB_PASSWORD
