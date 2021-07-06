65f17afae1d1965d9173132d928e84c71e6a9d36
Mark Turner <mark.turner-7@postgrad.manchester.ac.uk>
2021-07-06 17:53:29 +0100

    Fixed 'Can't open build.sh' error

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
-cp /etc/letsencrypt/live/app.markturner.uk/privkey.pem privkey.pem
-cp /etc/letsencrypt/live/app.markturner.uk/fullchain.pem fullchain.pem
+cp /etc/letsencrypt/live/app.markturner.uk/privkey.pem ceah-backend/privkey.pem
+cp /etc/letsencrypt/live/app.markturner.uk/fullchain.pem ceah-backend/fullchain.pem
 docker stop ceah-backend
 docker rm ceah-backend
 docker rmi -f ceah-backend
-docker build -t ceah-backend . --no-cache
+docker build -t ceah-backend ./ceah-backend --no-cache
